30a9afcdaf760b5298c5524e10d6a0db0203deca
Smartphone-Hackers <anandhcivil94956@gmail.com>
2023-01-06 12:42:00 +0530

    Add Exist

--- mio/settings.py
+++ mio/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'mio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mio_prod',
+        'USER': 'dbadmin',
+        'PASSWORD': 'Mio.me@94956!Mio',
+        'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -127,3 +136,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except Exception as e:
+    pass
